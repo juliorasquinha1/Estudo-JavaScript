dea4967e069de8b30d9c3117c620615bdbcfdc22
juliorasquinha1 <juliorasquinha@gmail.com>
2023-11-30 11:56:39 -0800

    iniciando no java,e o pesadelo sem fim

---
+++ matrizes.js
@@ -0,0 +1,5 @@
+const person = {firstname:"jhon",lastname:"doe",age:46};
+const person2 = ["kelly","silva",20];
+
+document.getElementById("jhon").innerHTML=person.firstname +" " +person.lastname + " "+ person.age;
+document.getElementById("kelly").innerHTML=person2[0]+" "+person2[1]+" "+person2[2];
