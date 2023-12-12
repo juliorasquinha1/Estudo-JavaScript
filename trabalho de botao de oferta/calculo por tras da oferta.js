afaea6d426e359049b039db95fd357a0c6cc20e7
juliorasquinha1 <juliorasquinha@gmail.com>
2023-12-12 10:58:47 -0800

    botao de oferta

---
+++ trabalho de botao de oferta/calculo por tras da oferta.js
@@ -0,0 +1,17 @@
+function inicio(){
+    let produto = window.prompt('digite o produto que voce quer comprar:');
+    let preço = parseFloat(window.prompt('digite o preço do produto R$:')); 
+    let desconto = parseFloate(window.prompt('digite o desconto que o produto esta tendo'));
+    let valor_do_desconto = (preço)*(desconto) / 100
+    let preço_com_desconto = (preço) - (valor_do_desconto)
+    let res = window.document.getElementById('resultado2');
+    res.innerHTML =
+    `<p>calculando o desconto de 10% para ${produto}
+   
+   <p> o preço original era R$${preço},00
+    
+    <p>voce acaba de ganhar R$${valor_do_desconto}, de desconto (-10%)
+
+    <p> valor final foi de R$${preço_com_desconto},00</p>`
+
+}
