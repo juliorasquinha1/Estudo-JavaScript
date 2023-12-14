815ec0ab25718a9713a1dee4ffb85f4130ec3136
juliorasquinha1 <juliorasquinha@gmail.com>
2023-12-14 12:03:15 -0800

    tabela

---
+++ tabela de tradução.html/traduções.js
@@ -0,0 +1,42 @@
+function TRADUCAO1() {
+    let res = window.document.getElementById('resultado');
+    res.innerHTML = `
+       
+            <table>
+                <tr>
+                    <th>Inglês</th>
+                    <th>Português</th>
+                </tr>
+
+            </table>
+
+            <table>
+
+            <td> 
+
+              main<br>
+              color<br>
+              let<br>
+              div<br>
+              transition<br>
+              body<br>
+              font-size<br>
+              const<br>
+              
+
+
+            <td> 
+
+              principal<br> 
+              cor<br> 
+              deixar<br>
+              divisão<br>
+              transição<br>
+              corpo<br>
+              tamanho da fonte<br>
+              constante<br>
+
+              </td>
+            </table>
+        `;
+}
